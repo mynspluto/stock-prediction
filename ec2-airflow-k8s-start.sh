08f15568d84333b6530254001e028a0907640741
mynspluto <mynspluto@gmail.com>
2025-02-02 00:44:31 +0900

    port-forward로 노출

--- ec2-airflow-k8s-start.sh
+++ ec2-airflow-k8s-start.sh
@@ -14,21 +14,20 @@ helm upgrade --install airflow apache-airflow/airflow -n airflow -f ./airflow/va
 
 sleep 5
 
-kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "NodePort", "ports": [{"port": 8080, "targetPort": 8080, "nodePort": 31000}]}}'
+#kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "NodePort", "ports": [{"port": 8080, "targetPort": 8080, "nodePort": 31000}]}}'
 #kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "ClusterIP", "ports": [{"port": 8080, "targetPort": 8080}]}}'
+PORT=8080
 
-# PORT=8080
+# Check if the port is in use and get the PID
+PID=$(lsof -t -i :$PORT)
 
-# # Check if the port is in use and get the PID
-# PID=$(lsof -t -i :$PORT)
+# If the port is in use, kill the process
+if [ -n "$PID" ]; then
+    echo "Port $PORT is already in use by PID $PID. Terminating the process..."
+    kill $PID
+    sleep 2  # Wait for the process to terminate
+fi
 
-# # If the port is in use, kill the process
-# if [ -n "$PID" ]; then
-#     echo "Port $PORT is already in use by PID $PID. Terminating the process..."
-#     kill $PID
-#     sleep 2  # Wait for the process to terminate
-# fi
+sleep 10
 
-# sleep 10
-
-# nohup kubectl port-forward svc/airflow-webserver $PORT:$PORT -n airflow > port-forward.log 2>&1 &
+nohup kubectl port-forward svc/airflow-webserver $PORT:$PORT -n airflow > port-forward.log 2>&1 &
