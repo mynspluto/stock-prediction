5fdb3d4bdfb7e4227f9250f5588fb0f01e861c48
mynspluto <mynspluto@gmail.com>
2025-02-02 00:35:01 +0900

    ingress 제거 nodeport로 노출

--- ec2-airflow-k8s-start.sh
+++ ec2-airflow-k8s-start.sh
@@ -9,19 +9,13 @@ docker build -t mynspluto-airflow:latest -f ./airflow/ec2-Dockerfile ./airflow
 
 helm repo add airflow-stable https://airflow-helm.github.io/charts
 helm repo add apache-airflow https://airflow.apache.org
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install nginx-ingress ingress-nginx/ingress-nginx
 helm upgrade --install airflow apache-airflow/airflow -n airflow -f ./airflow/values.yml
 
 sleep 5
 
-kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "ClusterIP", "ports": [{"port": 8080, "targetPort": 8080}]}}'
-
-minikube addons enable ingress
-minikube addons enable ingress-dns
-
-kubectl apply -f ./airflow/ingress.yml
+kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "NodePort", "ports": [{"port": 8080, "targetPort": 8080, "nodePort": 31000}]}}'
+#kubectl patch svc airflow-webserver -n airflow -p '{"spec": {"type": "ClusterIP", "ports": [{"port": 8080, "targetPort": 8080}]}}'
 
 # PORT=8080
 
