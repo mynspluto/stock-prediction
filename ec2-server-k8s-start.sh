4590091e872c8339064e8b6df09ce176be088719
mynspluto <mynspluto@gmail.com>
2025-02-23 15:47:58 +0900

    - node 설치 - fastapi 서버 포트포워딩

--- ec2-server-k8s-start.sh
+++ ec2-server-k8s-start.sh
@@ -1,5 +1,3 @@
-#!/bin/zsh
-
 kubectl create namespace fastapi
 kubectl config set-context --current --namespace=fastapi
 
@@ -7,4 +5,11 @@ eval $(minikube -p minikube docker-env)
 # unset DOCKER_HOST
 docker build -t api-server:latest -f ./api-server/ec2-Dockerfile ./api-server
 
+# kubectl port-forward pod/stock-prediction-api 8000:8000
+# nohup kubectl port-forward --address 0.0.0.0 -n fastapi pod/stock-prediction-api 8000:8000 > fastapi-portforward.log 2>&1 &
+
 kubectl apply -f ./api-server/dep.yml -n fastapi
+
+sleep 5
+
+nohup kubectl port-forward --address 0.0.0.0 -n fastapi pod/stock-prediction-api 8000:8000 > fastapi-portforward.log 2>&1 &
