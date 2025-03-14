33851eb7ab41bd02d22be3ad4a34fabb859fe06d
mynspluto <mynspluto@gmail.com>
2025-03-14 21:46:33 +0900

    ingress 주소 변경

--- next-stop.sh
+++ next-stop.sh
@@ -3,3 +3,5 @@ kubectl config set-context --current --namespace=web
 
 kubectl delete all --all -n web
 kubectl delete -f ./web/dep.yml -n web
+kubectl delete -f ./web/svc.yml -n web
+kubectl delete -f ./web/ingress.yml -n web
