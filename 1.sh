3db77dcbd529630a03e555b39dba71049e4d6bf6
mynspluto <mynspluto@gmail.com>
2025-04-23 10:18:34 +0900

    next 스크립트 제거

--- 1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-python3.12 -m venv ./airflow/airflow_env
-source ./airflow/airflow_env/bin/activate
-pip install --upgrade pip
-#pip install -r ./airflow-local/requirements.txt
-pip install numpy pandas scikit-learn yfinance requests hdfs apache-airflow pydantic fastapi
-python3.12 ./airflow/dags/1.py
--- next-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-kubectl config set-context --current --namespace=web
-
-kubectl delete all --all -n web
-kubectl delete -f ./web/dep.yml -n web
-kubectl delete -f ./web/svc.yml -n web
-kubectl delete -f ./web/ingress.yml -n web
