5b7b120934801d093efa0850abd62b8a45adae5a
mynspluto <mynspluto@gmail.com>
2025-04-23 10:14:46 +0900

    ec2 스크립트 제거

--- ec2-minikube-start.sh
+++
@@ -1,9 +0,0 @@
-minikube start --cpus 3 --memory 12000 --driver=docker
-minikube addons enable metrics-server
-minikube addons enable ingress
-minikube addons enable ingress-dns
-minikube docker-env
-eval $(minikube -p minikube docker-env)
-unset DOCKER_HOST
-
-minikube addons list | grep ingress
